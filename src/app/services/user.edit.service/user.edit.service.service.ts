bc5cdf51cf699c682109ffbf3aa59f10059de1b1
Leonardo <leonardo.alcedo@saggee.cl>
2025-06-06 23:54:18 -0400

    corrigo peticones

--- src/app/services/user.edit.service/user.edit.service.service.ts
+++ src/app/services/user.edit.service/user.edit.service.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class UserEditServiceService {
 
-  private API_URL = "http://0.0.0.0:6060";
+  private API_URL = "http://localhost:6060";
   readonly http = inject(HttpClient)
 
   editUser(user: any): Observable<any> {
